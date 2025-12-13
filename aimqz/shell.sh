1c4d62d978c9bc6cbad616c882458954d541f102
Dacong <372454567@qq.com>
2025-12-13 12:19:04 +0800

    shell

--- aimqz/shell.sh
+++ aimqz/shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 安装 Pytorch (假设你已经安装了 CUDA 11.8 或 12.x)
-pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --index-url https://download.pytorch.org/whl/cu126
 
 # 安装 Hugging Face 生态组件
 # trl: 专用于SFT微调的库
@@ -9,6 +9,8 @@ pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https
 # bitsandbytes: 量化库
 # accelerate: 多卡分布式训练
 pip install transformers datasets peft bitsandbytes trl accelerate pyyaml
+
+pip install nvidia-ml-py
 
 # 安装 flash-attn (可选，但在4090上推荐，能显著加速)
 pip install flash-attn --no-build-isolation
